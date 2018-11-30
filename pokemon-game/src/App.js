42d8637972cdb82c5e49d3621458d500f2119532
jheinz1999 <jonathan.heinz@aol.com>
2018-11-29 16:12:11 -0800

    tilemap now renders

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Phaser from 'phaser';
+
+import './App.scss';
 
 class App extends Component {
 
   constructor() {
+
+    super();
+
+    this.state = {
+
+      phaserConfig: {
+
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        parent: 'main-app',
+        scene:  {
+
+          preload: this.preload,
+          create: this.create,
+          update: this.update
+
+        }
+
+      },
+
+      game: {}
+
+    }
+
+  }
+
+  componentDidMount() {
+
+    this.setState({game: new Phaser.Game(this.state.phaserConfig)});
+
+  }
+
+  preload = function() {
+
+    console.log(this);
+
+    this.load.tilemapTiledJSON('hometown', 'assets/hometown.json');
+
+    this.load.image('tiles', 'assets/img/tileset.png');
+
+  }
+
+  create = function() {
+
+    let map = this.add.tilemap('hometown');
+
+    let tileset = map.addTilesetImage('hometown','tiles');
+
+    let layer = map.createStaticLayer('Tile Layer 1', tileset);
+
+  }
+
+  update = () => {
 
 
 
@@ -12,7 +67,7 @@ class App extends Component {
 
   render() {
     return (
-      <div></div>
+      <div id='main-app'></div>
     );
   }
 }
